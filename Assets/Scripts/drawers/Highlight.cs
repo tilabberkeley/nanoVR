3e3266b572ebe6cc16fee79c7c225b38b7fdfdbc
olliep24 <odpetrick@berkeley.edu>
2023-06-13 18:41:21 -0500

    Removed HighlightGO script. Fixed missing behaviour warning

--- Assets/Scripts/drawers/Highlight.cs
+++ Assets/Scripts/drawers/Highlight.cs
@@ -9,18 +9,30 @@ public static class Highlight
     private static Color highlightColor = Color.green;
     private static Color unhighlightColor = Color.black;
 
+    /// <summary>
+    /// Highlights given nucleotide.
+    /// </summary>
+    /// <param name="go">Nucleotide GameObject</param>
     public static void HighlightNucleotide(GameObject go)
     { 
         NucleotideComponent comp = go.GetComponent<NucleotideComponent>();
         comp.Highlight(highlightColor);
     }
 
+    /// <summary>
+    /// Unhighlights given nucleotide.
+    /// </summary>
+    /// <param name="go">Nucleotide GameObject</param>
     public static void UnhighlightNucleotide(GameObject go)
     {
         NucleotideComponent comp = go.GetComponent<NucleotideComponent>();
         comp.Highlight(unhighlightColor);
     }
 
+    /// <summary>
+    /// Highlights given list of nucleotides and backbones.
+    /// </summary>
+    /// <param name="list">GameObject list of nucleotides and backbones.</param>
     public static void HighlightNucleotideSelection(List<GameObject> list)
     {
         if (list == null)
@@ -42,6 +54,10 @@ public static class Highlight
         }
     }
 
+    /// <summary>
+    /// Unhighlights given list of nucleotides and backbones.
+    /// </summary>
+    /// <param name="list">GameObject list of nucleotides and backbones.</param>
     public static void UnhighlightNucleotideSelection(List<GameObject> list)
     {
         if (list == null)
